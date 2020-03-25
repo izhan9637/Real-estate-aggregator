3578d3e545092e9dde6327e15bd76ecaa014ae01
izhan9637 <izhan9637@gmail.com>
2020-03-26 00:59:24 +0530

    Added listing and realtor models

--- real_estate_project/real_estate_project/settings.py
+++ real_estate_project/real_estate_project/settings.py
@@ -116,3 +116,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'real_estate_project/static')
 ]
+
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
