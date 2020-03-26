64d341e784fe059e43c792287af007361d6c7753
izhan9637 <izhan9637@gmail.com>
2020-03-26 15:26:47 +0530

    Added contect model

--- real_estate_project/real_estate_project/settings.py
+++ real_estate_project/real_estate_project/settings.py
@@ -131,6 +131,12 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
+#EMAIL_HOST = 'smtp.gmail.com'
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = ''
+#EMAIL_HOST_PASSWORD= ''
+#EMAIL_USE_TLS = ''
+
 try:
     from .local_settings import *
 except ImportError:
